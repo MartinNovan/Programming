b41d76ee3329deba1eb8b4f69e63c68cf78df319
Martin Novan <116807577+MartinNovan@users.noreply.github.com>
2024-09-03 08:36:50 +0200

    práce v 1. a 2. hodině programování

---
+++ C#/Prg/01_prace_v_hodine/Počítání/Počítání/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pocitani // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10; int y = 4; float z = 4.5f;
+            int soucet = x + y;
+            int rozdil = x - y;
+            int soucin = x * y;
+            int celoCiselnyPodil = x / y; //podíl celého čísla
+            float DesetinyPodil = x / z;
+            Console.WriteLine(soucet);
+            Console.WriteLine(rozdil);
+            Console.WriteLine(soucin);
+            Console.WriteLine(celoCiselnyPodil);
+            Console.WriteLine(DesetinyPodil);
+
+            int vysledek = 2 * 3 + 5 * 4;
+            Console.WriteLine(vysledek);
+        }
+    }
+}
---
+++ C#/Prg/01_prace_v_hodine/SpecSymbol/SpecSymbol/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SpecSymbol // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"hoj\"");
+            Console.WriteLine("M:\\programovani\\Hoj"); //vystup M:\programovani\Hoj
+        }
+    }
+}
