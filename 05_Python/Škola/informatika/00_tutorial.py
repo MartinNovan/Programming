9af4f1273e334e756adedbb74570749028177ae9
Martin Novan <116807577+MartinNovan@users.noreply.github.com>
2024-09-30 13:13:41 +0200

    Začátek programování v pythonu v informatice
    nahrávám soubor tutorial a main, kde v tutorialu jsou vypsany nějáké základní datové typy a v main je nějáký jednodušší prográmek

---
+++ 05_Python/Škola/informatika/00_tutorial.py
@@ -0,0 +1,57 @@
+# Integer variable
+my_int = 10
+
+# Float variable
+my_float = 3.14
+
+# Complex variable
+my_complex = 2 + 3j
+
+
+# String variable
+my_string = "Hello, world!"
+
+
+# Boolean variable
+my_bool = True
+
+
+# List variable
+my_list = [1, 2, 3]
+
+
+# Tuple variable
+my_tuple = (4, 5, 6)
+
+
+# Range variable
+my_range = range(5)
+
+
+# Set variable
+my_set = {1, 2, 3}
+
+
+# Frozen set variable
+my_frozenset = frozenset([4, 5, 6])
+
+
+# Dictionary variable
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+
+# NoneType variable
+my_none = None
+
+
+# Bytes variable
+my_bytes = b'Hello'
+
+
+# Bytearray variable
+my_bytearray = bytearray(5)
+
+
+# Memoryview variable
+my_memoryview = memoryview(bytes(5))
+
