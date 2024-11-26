2d6edd21f8f709f68dc041fb177d3d8a5f0d08f2
Martin Novan <116807577+MartinNovan@users.noreply.github.com>
2024-11-26 18:52:17 +0100

    Uploaded assigment 10 from Application Development
    In this assignment, the task was to create a primitive text editor that can underline, strikethrough and overline text in a textbox. It also has to highlight errors, wrap text and insert line breaks.

---
+++ 10_C#/School/2024-2025/Application development/WPF/10_assigment/10_assigment/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+
+namespace _10_assigment;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void UnderlineOnChecked(object sender, RoutedEventArgs e)
+    {
+        SetTextDecoration(TextDecorations.Underline);
+    }
+
+    private void OverlineOnChecked(object sender, RoutedEventArgs e)
+    {
+        SetTextDecoration(TextDecorations.OverLine);
+    }
+
+    private void StrikethroughOnChecked(object sender, RoutedEventArgs e)
+    {
+        SetTextDecoration(TextDecorations.Strikethrough);
+    }
+
+    private void CheckBoxUnchecked(object sender, RoutedEventArgs e)
+    {
+        if (UnderlineCheckBox.IsChecked != null && !UnderlineCheckBox.IsChecked.Value 
+            && OverlineCheckBox.IsChecked != null && !OverlineCheckBox.IsChecked.Value 
+            && StrikethroughCheckBox.IsChecked != null && !StrikethroughCheckBox.IsChecked.Value)
+        {
+            TextBox.TextDecorations = null;
+        }
+    }
+
+    private void SetTextDecoration(TextDecorationCollection decoration)
+    {
+        TextBox.TextDecorations = decoration;
+
+        if (decoration == TextDecorations.Underline)
+        {
+            OverlineCheckBox.IsChecked = false;
+            StrikethroughCheckBox.IsChecked = false;
+        }
+        else if (decoration == TextDecorations.OverLine)
+        {
+            UnderlineCheckBox.IsChecked = false;
+            StrikethroughCheckBox.IsChecked = false;
+        }
+        else if (decoration == TextDecorations.Strikethrough)
+        {
+            UnderlineCheckBox.IsChecked = false;
+            OverlineCheckBox.IsChecked = false;
+        }
+    }
+}
