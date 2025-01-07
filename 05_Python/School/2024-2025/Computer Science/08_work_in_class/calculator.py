80f0418ce6f435b57174e8b5f42afaaeb8f96122
Martin Novan <116807577+MartinNovan@users.noreply.github.com>
2025-01-07 15:53:31 +0100

    Uploaded assigment 12
    This assigment is specialized in creating styles. More in README.md

---
+++ 05_Python/School/2024-2025/Computer Science/08_work_in_class/calculator.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Nelze delit nulou!")
+    return a / b
