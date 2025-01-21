191e94d3f034b9b5798a12b12bcacfb3f742184b
Martin Novan <116807577+MartinNovan@users.noreply.github.com>
2025-01-21 21:56:06 +0100

    Uploaded assigment 14 from application development class
    In this assigment we were supposed to learn how to create and use new windows and pages in WPF framework. I took it step further and make decent logging app (decent for like 2 hours of programming, in reality this is bad app), that stores accounts in JSON file and hashes the password. App also customize itself with dark or white theme by what is your config in windows. (It uses registry value to determinate which theme to use.). More in README.md

---
+++ 10_C#/School/2024-2025/Application development/WPF/14_assigment/14_assigment/Pages/LoginPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using _14_assigment.Windows;
+using static _14_assigment.MainWindow;
+
+namespace _14_assigment.Pages;
+
+public partial class LoginPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Hyperlink_OnClick(object sender, RoutedEventArgs e)
+    {
+        new RegisterWindow().Show();
+    }
+
+    private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            if (UsernameEntry.Text.Length == 0 || PasswordEntry.Text.Length == 0)
+            {
+                MessageBox.Show("Please fill in all fields!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            bool correctLogin = await new FileHelper().Login(UsernameEntry.Text, PasswordEntry.Text);
+            if (correctLogin)
+            {
+                NavigateToPage(new MainPage());
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+}
---
+++ 10_C#/School/2024-2025/Application development/WPF/14_assigment/14_assigment/Pages/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace _14_assigment.Pages;
+
+public partial class MainPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
