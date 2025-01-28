44bc834c5d7448919f8792636f156addf4665a53
Martin Novan <116807577+MartinNovan@users.noreply.github.com>
2025-01-28 14:40:59 +0100

    Uploaded assigment 15
    assigment 15 is alternative to assigment 14 (instead of using JSON file for storing data, we are using SQLite database)

---
+++ 10_C#/School/2024-2025/Application development/WPF/15_assigment/15_assigment/UserControls/PlaceHolderTextBox.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace _15_assigment.UserControls;
+
+public partial class PlaceHolderTextBox
+{
+    public static readonly DependencyProperty PlaceholderProperty = DependencyProperty.Register(nameof(Placeholder), typeof(string), typeof(PlaceHolderTextBox), new PropertyMetadata(""));
+    public string Placeholder
+    {
+        get => (string)GetValue(PlaceholderProperty);
+        set => SetValue(PlaceholderProperty, value);
+    }
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(PlaceHolderTextBox), new PropertyMetadata(""));
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(PlaceHolderTextBox), new PropertyMetadata(new CornerRadius(0)));
+    public string CornerRadius
+    {
+        get => (string)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
+    public PlaceHolderTextBox()
+    {
+        InitializeComponent();
+    }
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var textbox = (TextBox)sender;
+        PlaceholderElement.Visibility = !string.IsNullOrEmpty(textbox.Text) ? Visibility.Collapsed : Visibility.Visible;
+    }
+}
---
+++ 10_C#/School/2024-2025/Application development/WPF/15_assigment/15_assigment/Windows/RegisterWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace _15_assigment.Windows;
+
+public partial class RegisterWindow
+{
+    public RegisterWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        if(UsernameEntry.Text.Length == 0 || EmailEntry.Text.Length == 0 || PasswordEntry.Text.Length == 0 || PasswordConfirmationEntry.Text.Length == 0)
+        {
+            MessageBox.Show("Please fill in all fields!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+        if (PasswordEntry.Text != PasswordConfirmationEntry.Text)
+        {
+            MessageBox.Show("Passwords do not match", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+        var user = new Models.User
+        {
+            Username = UsernameEntry.Text,
+            Email = EmailEntry.Text,
+            Password = PasswordEntry.Text,
+        };
+        new UserRepository().Register(user);
+        Close();
+    }
+}
